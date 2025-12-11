dafaec7244e9ffc7e20e084cbf582eaaed702246
huadyng <huydang882125@gmail.com>
2025-12-11 15:29:25 +0700

    Initial commit frontend5

--- frontend/src/component/API/FinalReportAPI.js
+++
@@ -1,9 +0,0 @@
-import httpClient from "./httpClient";
-
-const finalReportApi = {
-  create: (data) => httpClient.post("/Finalreport/create", data),
-  update: (data) => httpClient.put("/Finalreport/update", data),
-  getAll: () => httpClient.get("/Finalreport/getAll"), // thêm GET tất cả report
-};
-
-export default finalReportApi;
